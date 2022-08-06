d6a462b7e93106fcb2d8af8a139a80e180116c18
RamoIsReaLoL <85399076+RamoIsReaLoL@users.noreply.github.com>
2022-08-07 00:51:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const { Client, Intents } = require("discord.js");
+const fs = require("fs");
+const client = new Client({ intents: 851 });
+const config = require("./config.json");
+
+const synchronizeSlashCommands = require("discord-sync-commands");
+
+const { token } = require("./config.json");
+client.commands = new Collection();
+fs.readdir("./komutlar/", (_err, files) => {
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./komutlar/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, {
+      name: commandName,
+      ...props,
+    });
+    console.log(`Komut Yüklendi: ${commandName}`);
+  });
+  synchronizeSlashCommands(
+    client,
+    client.commands.map((c) => ({
+      name: c.name,
+      description: c.description,
+      options: c.options,
+      type: c.type,
+    })),
+    {
+      debug: true,
+    }
+  );
+});
+
+fs.readdir("./events/", (_err, files) => {
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`Event yüklendi: ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
+});
+
+client.login(config.token);
