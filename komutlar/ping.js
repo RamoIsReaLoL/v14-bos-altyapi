8a62eaecfdba8fe0d8bfcaeb9c5b5053de64f63d
RamoIsReaLoL <85399076+RamoIsReaLoL@users.noreply.github.com>
2022-08-08 23:16:06 +0300

    Update ping.js

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -1,10 +1,10 @@
 const { ApplicationCommandType } = require("discord.js");
 module.exports = {
+    name: 'ping',
     description: 'Botun Pingine Bakarsınız',
     type: ApplicationCommandType.CHAT_INPUT,
     options: [],
     run: async (client, interaction) => {
-       interaction.reply({ content: "Lütfen Bekleyin..." })
        interaction.channel.send(`${client.ws.ping} ms`)
  }
 };
