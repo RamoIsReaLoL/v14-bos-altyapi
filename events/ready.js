6f2e229ba1720819268542a5239e836329866ba1
RamoIsReaLoL <85399076+RamoIsReaLoL@users.noreply.github.com>
2022-08-16 00:34:50 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} ismi ile giriş yapıldı`);
-        client.user.setPresence({activities: [{name:'Glazy v14 | /yardım'}], status:'online'})
+        client.user.setPresence("Glazy v14 | /yardım")
     
 };
